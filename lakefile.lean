3bf50c67e3ae91cd850fefe241b9c383d4b6c0b4
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-12-12 18:23:26 +0200

    refactor `lakefile.lean`

--- lakefile.lean
+++ lakefile.lean
@@ -1,20 +1,7 @@
 import Lake
 open Lake DSL
 
-/- package "aoc2024-lean4" where -/
-package "aoc2024" where
-  -- add package configuration options here
-
-/- lean_lib «Aoc2024Lean4» where -/
-  -- add library configuration options here
-
-/- @[default_target] -/
-/- lean_exe "aoc2024-lean4" where -/
-/-   root := `Main -/
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  /- supportInterpreter := true -/
+package "aoc2024"
 
 lean_lib "aoc2024"
 
@@ -28,12 +15,6 @@ lean_exe "day2" where
 
 lean_exe "day11" where
   root := `aoc2024.day11.part1
-
 lean_exe "day11p2" where
   root := `aoc2024.day11.part2
-
-
-
-/- require mathlib @ "2077" from git -/
-/-   "https://github.com/leanprover-community/mathlib4" -/
 
