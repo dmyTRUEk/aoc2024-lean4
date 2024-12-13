9a17f6d15b2c2c30f41b4e9fc9e13a1c7f1d655a
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-12-13 12:19:08 +0200

    fix & refactor template

--- aoc2024/dayXX/solution.lean
+++ aoc2024/dayXX/solution.lean
@@ -1,7 +1,7 @@
 import aoc2024.mylib
 
 import aoc2024.dayXX.example
-import aoc2024.dayXX.input
+/- import aoc2024.dayXX.input -/
 
 
 
@@ -20,11 +20,11 @@ def solve_part_one (input : String) : Nat :=
 
 
 
-def solve_part2 (input : String) : Nat :=
+def solve_part_two (input : String) : Nat :=
     input
         |> parse_input
         |> sorry
 
-#eval solve_part_one example_2
-#guard example_2_answer == solve_part_one example_2
+#eval solve_part_two example_2
+#guard example_1_answer_part_two == solve_part_two example_1
 
